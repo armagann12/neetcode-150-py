58d9a74a1e5db63b9d854b4b49e541c323e00c9a
armagann12 <armagansisik@gmail.com>
2024-01-05 19:12:33 +0300

    1. Two Sum

---
+++ Arrays&Hashing/1. Two Sum.py
@@ -0,0 +1,7 @@
+def twoSum(self, nums, target):
+    map = {}
+    for i in range(len(nums)):
+        if target - nums[i] in map:
+            return [map[target - nums[i]], i]
+        map[nums[i]] = i
+    return []
