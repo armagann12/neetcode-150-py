0bdd5a05dc1aa70b239be53299f58c6670cf43f6
armagann12 <armagansisik@gmail.com>
2024-01-09 16:41:53 +0300

    347. Top K Frequent Elements

--- Arrays&Hashing/347. Top K Frequent Elements.py
+++ Arrays&Hashing/347. Top K Frequent Elements.py
@@ -8,3 +8,56 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
     resMap = dict(sorted(resMap.items(), key=lambda x: x[1], reverse=True))
     result = list(resMap.keys())[:k]
     return result
+
+# Using the hashmap like the first one but keys and values are reversed. Using sort of like bucket sort solution not sure
+# Needs refactoring
+
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    nums = sorted(nums)
+    count = 1
+    resMap = {}
+    result = []
+    for i in range(len(nums)):
+        if i == len(nums) - 1:
+            if count not in resMap:
+                resMap[count] = []
+            resMap[count].append(nums[i])
+        elif nums[i] == nums[i + 1]:
+            count += 1
+        else:
+            if count not in resMap:
+                resMap[count] = []
+            resMap[count].append(nums[i])
+            count = 1
+    resultKeys = list(sorted(resMap.keys(), reverse=True))[:k]
+    for keys in resultKeys:
+        for key in resMap[keys]:
+            result.append(key)
+            k -= 1
+            if k == 0:
+                return result
+    return result
+
+# Refactored neetcode version also using an array
+
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+
+    for num in nums:
+        count[num] = 1 + count.get(num, 0)
+    for n, c in count.items():
+        freq[c].append(n)
+
+    result = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            result.append(n)
+            if len(result) == k:
+                return result
+
+
+
+
+# You can also use heap
+
