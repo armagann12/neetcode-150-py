d3d38d5497747fd983a8f39e2c3e4e360829add4
armagann12 <armagansisik@gmail.com>
2024-01-13 23:08:34 +0300

    238. Product of Array Except Self

--- Arrays&Hashing/238. Product of Array Except Self.py
+++ Arrays&Hashing/238. Product of Array Except Self.py
@@ -31,3 +31,19 @@ def productExceptSelf(self, nums: List[int]) -> List[int]:
             post = postfix[i + 1]
         answer[i] = pre * post
     return answer
+
+# Prefix and Postfix without using extra 2 arrays:
+
+#Answer:
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    answer = [1] * len(nums)
+    prefix = 1
+    for i in range(len(nums)):
+        answer[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        answer[i] *= postfix
+        postfix *= nums[i]
+    return answer
+
