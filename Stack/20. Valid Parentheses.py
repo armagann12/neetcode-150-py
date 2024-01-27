93d6c9b2b278b40f3d0f8f7584e52418c56e351e
armagann12 <armagansisik@gmail.com>
2024-01-27 21:03:20 +0300

    20. Valid Parentheses

---
+++ Stack/20. Valid Parentheses.py
@@ -0,0 +1,24 @@
+# Doing a if else solution with a loop
+
+
+# Stack solution
+class Solution:
+    def isValid(self, s: str) -> bool:
+        res = {
+            "[": "]",
+            "{": "}",
+            "(": ")"
+        }
+        stack = []
+        for c in s:
+            if c == "[" or c ==  "(" or c == "{":
+                stack.insert(0, c)
+            else:
+                if len(stack) == 0:
+                    return False
+                if c != res.get(stack[0]):
+                    return False
+                stack.pop(0)
+        if len(stack) != 0:
+            return False
+        return True
