ad74aaf13f489bdbc67a87d238efe72852981a63
armagann12 <armagansisik@gmail.com>
2024-01-30 15:38:32 +0300

    155. Min Stack

---
+++ Stack/155. Min Stack.py
@@ -0,0 +1,20 @@
+# Push to Stack
+# Pop from the Stack
+# Get the top element in Stack
+# Get the min element in the Stack
+# using array / list in python
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
