9faa55a7a8ff43f2aef891573a1cf74336f214f4
armagann12 <armagansisik@gmail.com>
2024-01-30 15:43:25 +0300

    155. Min Stack

--- Stack/155. Min Stack.py
+++ Stack/155. Min Stack.py
@@ -18,3 +18,26 @@ class MinStack:
 
     def getMin(self) -> int:
         return min(self.stack)
+
+# Using also another list for calculating min val
+# Checking if its less than the val in the stack
+# If its not it just uses the same val as in the stack
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.min[-1] if self.min else val)
+        self.min.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
