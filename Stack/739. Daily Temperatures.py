98b4046e4ba938b09a6ce0a45266e5269d8f6dac
armagann12 <armagansisik@gmail.com>
2024-02-04 19:06:39 +0300

    739. Daily Temperatures

--- Stack/739. Daily Temperatures.py
+++ Stack/739. Daily Temperatures.py
@@ -1,4 +1,5 @@
 # Solution wihouth refactoring + Time limit exceeded
+# With using hashmap
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
@@ -19,5 +20,63 @@ class Solution:
             stack.append(hashMap)
 
         return res
-            
-            
+
+# Time limit exceded     
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        indexes = []
+        for i in range(len(temperatures)):
+            temp = temperatures[i]
+
+            if len(stack) != 0:
+                for j in range(len(stack)):
+                    if temp > stack[-1]:
+                        el = stack.pop()
+                        index = indexes.pop()
+                        res[index] = i - index
+            stack.append(temp)
+            indexes.append(i)
+
+        return res
+
+# Accepted solution
+# My solution
+    
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        indexes = []
+        for i in range(len(temperatures)):
+            temp = temperatures[i]
+
+            while len(stack) != 0 and temp > stack[-1]:
+                el = stack.pop()
+                index = indexes.pop()
+                res[index] = i - index
+            stack.append(temp)
+            indexes.append(i)
+
+        return res
+
+
+# Refactored 
+# Using only one list
+    
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            temp = temperatures[i]
+
+            while stack and temp > stack[-1][0]:
+                el = stack.pop()
+                res[el[1]] = i - el[1]
+            stack.append([temp, i])
+
+        return res
+
+
