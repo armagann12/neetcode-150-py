9a3938a684cf517a846f5a0df635a36a38ddac26
armagann12 <armagansisik@gmail.com>
2024-02-04 18:05:39 +0300

    Brute solution first draft

--- Stack/739. Daily Temperatures.py
+++ Stack/739. Daily Temperatures.py
@@ -1,14 +1,23 @@
+# Solution wihouth refactoring + Time limit exceeded
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
         stack = []
         for i in range(len(temperatures)):
-            if i == 0:
-                stack.append(temperatures[i])
-                continue
+            hashMap = {}
+            temp = temperatures[i]
+            hashMap[temp] = i
+            if len(stack) != 0:
+                for j in range(len(stack)):
+                    mp = stack[-1]
+                    keys = mp.keys()
+                    # Refactor this for loop
+                    for key in keys:
+                        if temp > key:
+                            stack.pop()
+                            res[mp[key]] = i - mp[key]
+            stack.append(hashMap)
 
-            if temperatures[i] > stack[-1]:
-                stack.pop()
-                res[i - 1] += 1
-            # It should check if stack is empty or not if not it should check for > again
-            stack.append(temperatures[i])
+        return res
+            
+            
