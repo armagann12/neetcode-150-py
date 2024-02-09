2b755efb90cc9ae36c39425a450a1ecde2592300
armagann12 <armagansisik@gmail.com>
2024-02-09 14:46:12 +0300

    75. Sort Colors

--- Two Pointers/Extras/75. Sort Colors.py
+++ Two Pointers/Extras/75. Sort Colors.py
@@ -10,6 +10,24 @@ class Solution:
                     tmp = nums[j]
                     nums[j] = nums[j + 1]
                     nums[j + 1] = tmp
-                
 
-        
+# My solution using hints
+# Using Counting array - two pass solutio
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Counting Sort
+        # An array as length of nums (3 for this question)
+        # and indexes are the nums values are the occurences
+        count = [0] * 3
+        for num in nums:
+            count[num] += 1
+
+        start = 0
+        for i in range(len(nums)):
+            while count[start] == 0:
+                start += 1
+            nums[i] = start
+            count[start] -= 1
