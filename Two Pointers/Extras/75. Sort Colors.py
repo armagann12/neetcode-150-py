7348965abf27d829507dfe56112dda08e073136a
armagann12 <armagansisik@gmail.com>
2024-02-13 00:07:05 +0300

    75. Sort Colors

--- Two Pointers/Extras/75. Sort Colors.py
+++ Two Pointers/Extras/75. Sort Colors.py
@@ -13,6 +13,8 @@ class Solution:
 
 # My solution using hints
 # Using Counting array - two pass solutio
+
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
@@ -31,3 +33,22 @@ class Solution:
                 start += 1
             nums[i] = start
             count[start] -= 1
+
+# MergeSort / Quicksort you can do
+
+
+# Using BucketSort
+# Because we know there is only 3 values
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = {0: 0, 1: 0, 2: 0}
+        for num in nums:
+            count[num] = count.get(num) + 1
+
+        index = 0
+        for c in count:
+            nums[index: index + count[c]] = [c] * count[c]
+            index += count[c]
