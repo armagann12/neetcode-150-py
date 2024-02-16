7765377fdf62cfc0f37ed039a18145b2b11c524b
armagann12 <armagansisik@gmail.com>
2024-02-16 19:43:06 +0300

    567. Permutation in String

--- Sliding Window/567. Permutation in String.py
+++ Sliding Window/567. Permutation in String.py
@@ -9,11 +9,11 @@ class Solution:
         count2 = {}
         for s in s2:
             count2[s] = count2.get(s, 0) + 1
-        return # count2 contains count1 or not
-    
+        return  # count2 contains count1 or not
+
 # Importtant when doing sliding window you have to start from the beggining you cant do l = 0 r = 3
 # because you have to calculate the between for that you need to start from beginning l = 0 r= 0 or 1
-    
+
 # TLE my solution can be refactored
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
@@ -31,8 +31,39 @@ class Solution:
                     r += 1
                 if newCount == count:
                     return True
-            l+= 1
-        
+            l += 1
+
+        return False
+
+# Using sorting
+# Checking every window in string that is length of s1
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if sorted(s1) == sorted(s2):
+            return True
+        s1 = sorted(s1)
+        l = 0
+        r = l + len(s1)
+        while r < len(s2):
+            r = l + len(s1)
+            arr = s2[l:r]
+            if sorted(arr) == s1:
+                return True
+            l += 1
+
+        return False
+
+
+# This is the actual thing
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        r = l + len(s1)
+        while r < len(s2):
+            r = l + len(s1)
+            arr = s2[l:r]
+            print(arr, s1, "is it a valid anagram")
+            l += 1
         return False
 
         
